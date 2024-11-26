63061a926d18fd1f25a549c5bb87afa11778fda0
yogeshrkatole@gmail.com <yogeshrkatole@gmail.com>
2024-11-26 15:22:08 +0530

    Played With Single Player

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,6 +1,9 @@
 
 public class SnakeAndLadder {
+    private static final int START_POSITION = 0;
 public static void main(String[] args) {
-	System.out.println("Welcome to Snake And Ladder Game");
+	System.out.println("Welcome to Snake And Ladder Game\n");
+	int playerPosition = START_POSITION;
+	System.out.println("Player starts at position: " + playerPosition);
 }
 }
