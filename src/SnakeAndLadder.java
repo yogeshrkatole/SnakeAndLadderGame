48fc17d1f2ae9a009eddd26dd9eabb4edb78e7a8
yogeshrkatole@gmail.com <yogeshrkatole@gmail.com>
2024-11-26 15:37:08 +0530

    Player Rolls the Die

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class SnakeAndLadder {
     private static final int START_POSITION = 0;
@@ -5,5 +6,8 @@ public static void main(String[] args) {
 	System.out.println("Welcome to Snake And Ladder Game\n");
 	int playerPosition = START_POSITION;
 	System.out.println("Player starts at position: " + playerPosition);
+	 Random random=new Random();
+	 int dieRoll=random.nextInt(6)+1;
+	 System.out.println("Player rolls the die and gets: "+dieRoll);
 }
 }
